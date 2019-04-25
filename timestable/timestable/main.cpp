30d9d6941a25c7e3a4194d8f9dd3155bd1c58787
LeeKyungHeon <leeghgood1234@gmail.com>
2019-04-25 17:08:54 +0900

    timestable

---
+++ timestable/timestable/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 2; i <= 9; i++)
+	{
+		for (int j = 1; j <= 9; j++)
+		{
+			cout << i << " * " << j << " = " << i * j << endl;
+
+			if (j % 9 == 0)
+			{
+				cout << endl;
+			}
+		}
+	}
+}
