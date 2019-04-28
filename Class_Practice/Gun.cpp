799cbee03b950bc4920c2dd649960c04775b2a61
Hyunsoul37 <49974797+Hyunsoul37@users.noreply.github.com>
2019-04-28 21:12:48 +0900

    Gun Class
    수정했습니다

--- Class_Practice/Gun.cpp
+++ Class_Practice/Gun.cpp
@@ -22,16 +22,25 @@ public:
 	{
 		if (Count == 0)	// 총알 0발일시 재장전
 		{
-			Reload(); // 재장전 함수
+			//Reload(); // 재장전 함수
+			Count = 0;
+			cout << "재장전이 필요해!!" << endl;				//0개가 되고 fire()함수를 호출시 다시 장전되어있는것을 바꿨습니다.
+			//GunInfo();
+			return;
 		}
 
 		if (Degree >= MAXDEGREE)	// 총 온도가 한계치 일 때
 		{
 			cout << "앗 뜨거! 식을때까지 기다려야 겠군..." << endl;
 
-			Sleep(3000);
+			int n = Degree / 10;
 
-			Degree = 0;
+			for (int i = 0; i < n; i++)						//총의 온도를 0도가 될때까지 내려줍니다.
+			{												//총의온도는 0.5초마다 10도씩 내려갑니다.
+				Sleep(500);
+				Degree -= 10;
+				GunInfo();
+			}
 		}
 
 		else
@@ -39,14 +48,22 @@ public:
 			cout << "빵야빵야!" << endl;
 			Count--;
 			Degree += 10;
+			GunInfo();
 		}
 
+		
+	}
+
+	void GunInfo() const									//총의 상태를 알려주는 함수를 따로 만들었습니다.
+	{
 		cout << "남은 총알개수 : " << Count << endl;
 		cout << "총 온도 : " << Degree << endl;
 	}
 
 	void Reload()
 	{
+		
+		
 		if (Count < 5)
 		{
 			cout << "제기랄 총알이 없잖아!! 어서 장전해야해!!" << endl;
